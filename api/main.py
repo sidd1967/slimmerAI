6645be2a17b3f4c0caf51b75bda7898e024a1882
sid nathan <siddhartha.ranganathan@gmail.com>
2020-11-13 19:10:37 +0000

    Final Commit

--- api/main.py
+++ api/main.py
@@ -1,11 +1,59 @@
-
 import time
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
+from flask_mysqldb import MySQL
+import json
 
-app= Flask(__name__)
+import spacy
+from typing import Tuple, List
+app = Flask(__name__)
+CORS(app)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'sid'
+app.config['MYSQL_PASSWORD'] = '1967'
+app.config['MYSQL_DB'] = 'feedback'
+
+mysql = MySQL(app)
+
+nlp = spacy.load("en_core_web_sm")
 
 
+@app.route('/feedback', methods = ["POST", "GET"])
+def get_feedback():
+      if request.method == "POST":
+            req_data = request.get_json()
+            fb1 = req_data['fb']
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO myfeedback1 (Value) VALUES (%s)", [fb1] )
+            mysql.connection.commit()
+            cur.close()
+            return {'success': "success"}
 
-@app.route('/main')
-def get_current_time():
-      return {'time': time.time()}
+@app.route('/text', methods = ["POST", "GET"])
+def get_data():
+      if request.method == "POST":
+            req_data = request.get_json()
+            txt = req_data['txt']
+
+            processed_text = process_text(txt)
+
+            return {"final_data": processed_text}
+
+
+def process_text(text: str) -> List[Tuple[str, str]]:
+	"""Processes the text and reslimmturns the recognized entities
+	
+	:param text: The input for the model
+	:return: returns a list with a tuple for each word in the text containing
+			 the text itself, and the entity type if found, or empty string
+			 if not
+	"""
+
+	doc = nlp(text)
+
+	result = []
+	for word in doc:
+		result.append((str(word), word.ent_type_))
+
+	return result
--- src/App.js
+++ src/App.js
@@ -1,22 +1,137 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from "axios";
+import Table from 'react-bootstrap/Table'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card'
+import Badge from 'react-bootstrap/Badge'
+
+const Result = props => (
+  <tr>
+      <td>{props.res[0]}</td>
+      <td>{props.res[1]}</td>
+      
+
+  </tr>
+)
 
 function App() {
+
+  const [finalOutput, setFinalOutput] = useState([]);
+
+
+
+function buttonClick(e){
+    e.preventDefault();
+    var value = e.target.toggle.value;
+    console.log(value); 
+    const requestOptions = {
+      fb: value
+    }
+    axios.post('http://localhost:5000/feedback', requestOptions)
+    .then(res => {
+      window.alert("Recorded in DB");
+    })
+
+}
+
+
+function displayResult(){
+  return finalOutput.map(function(cur, i){
+    console.log(cur);
+    <span className="badge badge-secondary p-2">
+    {cur}
+    </span>
+
+      return <Result res={cur} key={i} />;
+  })
+}
+
+
+
+
+
+ function handleClick(e){
+   e.preventDefault();
+   var textValue = e.target.text.value;
+   //console.log(textValue);
+   const requestOptions = {
+     txt: textValue
+   }
+ 
+    axios.post('http://localhost:5000/text', requestOptions)
+    .then(res => {
+      setFinalOutput(res.data.final_data);
+      //console.log(typeof(res.data.final_data[0]));
+     
+      //console.log(data.data.final_data[0][0]);
+    });
+    
+
+ }
   return (
+
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
+      <br/><br/>
+   <h1>NAMED ENTITY RECOGNITION</h1><br/>
+      <form id="form1" onSubmit= {handleClick}>
+      <p>Enter Text:</p>
+      <textarea id="w3review" name="text" rows="4" cols="50" /> <br/>
+      <br/>
+      <Button type="submit"  size="lg" value="process"  className="button" >Submit</Button><br/> <br/> 
+        </form>
+        <h2>Output:</h2>
+        {[          
+          'Secondary'
+          
+].map((variant, idx) => (
+  <Card bg={variant.toLowerCase()} border="secondary" text={variant.toLowerCase() === 'light' ? 'dark' : 'white'} style={{ width: '50rem' }}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+              {finalOutput.map((val, key) => (
+                <span key={key} className="badge badge-light ml-3 p-10" display = "block">
+                  
+                  {val[0]} <span style={{color: "red", fontSize: "16px"}}>{val[1]}</span>
+
+                </span>
+                ))}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    </Card>))}
+ <br/>
+       <form id= "feedback" onSubmit = {buttonClick}>
+       <p>Is the Recognition Correct?</p> 
+       <span>
+        <label htmlFor="toggle-on"><Badge variant="success"  style={{ margin:"10px" }} >YES</Badge>  </label><input id="toggle-on" name="toggle" value = "YES" type="radio" />
+        <br/>
+        <label htmlFor="toggle-off"><Badge variant="danger" style={{ margin:"10px" }} >NO</Badge>  </label>  <input id="toggle-off" name="toggle" value = "NO" type="radio" />
+        
+
+       </span>
+        <br/>
+        
+        <Button type="submit"  value="process" size="lg" className="button" >Submit</Button><br/><br/><br/><br/> 
+
+       </form>
+       <h1>OUTPUT TABLE</h1>
+                <Table striped bordered hover variant="dark">
+
+                    <thead>
+                        <tr>
+                            <th>Text</th>
+                            <th>Result</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { displayResult() }
+                    </tbody>
+          </Table>
+
+     
+       
       </header>
     </div>
   );
